740ccf2eb225b8fee4ca2e1f13f31d970a379289
Regala-git <mregala427@gmail.com>
2026-02-14 12:20:14 +0800

    Initial commit

---
+++ samiaCute/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SamiaLuck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string num;
+            Console.WriteLine("Please Enter a Number between 1 and 5 and find the lucky number");
+            num = Console.ReadLine();
+
+            if (num == "3")
+            {
+                Console.WriteLine("It's your lucky day!! You got the lucky number!!");
+            }
+            else
+            {
+                Console.WriteLine("Better luck next time!");
+            }
+        }
+    }
+}
